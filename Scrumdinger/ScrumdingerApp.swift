7a40c359baf77bcc26f9a9b5c5a839e3dc0d05d7
Md. Albin Hossain <md.albin.hossain@icloud.com>
2025-02-11 16:16:50 +0600

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Albin on 11/2/25.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
